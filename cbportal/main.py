9bbc3b44bc96fe9f7d4b759998d55c20f6cb6896
Matthieu Crépeau <creposukre@gmail.com>
2024-04-20 10:57:50 -0500

    Fixed bugs, refactored code

--- cbportal/main.py
+++ cbportal/main.py
@@ -9,6 +9,7 @@ import threading
 import paho.mqtt.client as mqtt
 import pyperclip
 import argparse
+import logging
 if platform.system() == "Windows":
     import win32clipboard
 
@@ -24,21 +25,30 @@ from .utils import create_key_from_password
 from .version import __version__
 
 
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+
+
 class MQTTClientWithClipboard:
     def __init__(self, client, topic, cipher):
         self.client = client
         self.topic = topic
         self.cipher = cipher
         self.last_content_hash = None
+        self.no_recv = False
         self.client.on_message = self.on_message
         self.client.on_connect = self.on_connect
 
     # The callback for when the client receives a CONNACK response from the server.
     def on_connect(self, client, userdata, flags, reason_code, properties):
-        print(f"Connected with {reason_code}")
-        # Subscribing in on_connect() means that if we lose the connection and
-        # reconnect then subscriptions will be renewed.
-        client.subscribe(self.topic)
+        if reason_code == 0:
+            logging.info("Connected successfully to broker")
+            # Subscribing in on_connect() means that if we lose the connection and
+            # reconnect then subscriptions will be renewed.
+            if not self.no_recv:
+                client.subscribe(self.topic)
+        else:
+            logging.error(f"Failed to connect to broker: {reason_code}")
 
     def on_message(self, client, userdata, message):
         clipboard_payload = ClipboardPayload.from_json(message.payload.decode())
@@ -47,12 +57,12 @@ class MQTTClientWithClipboard:
         try:
             decrypted_clipboard_content = self.cipher.decrypt(encrypted_clipboard_content)
         except InvalidToken:
-            print("Could not decrypt clipboard. The content may have been tampered with or the wrong key was used.")
+            logging.error("Could not decrypt clipboard. The content may have been tampered with or the wrong key was used.")
             return None
 
         if clipboard_hash != self.last_content_hash:
             if clipboard_payload.type == 'image':
-                print("Clipboard content (image) received from portal!")
+                logging.info("Clipboard content (image) received from portal!")
                 image_data = base64.b64decode(decrypted_clipboard_content)
                 image = Image.open(io.BytesIO(image_data))
                 # Save the image to a temporary file
@@ -80,7 +90,7 @@ class MQTTClientWithClipboard:
                 # Delete the temporary file
                 os.unlink(temp.name)
             else:
-                print("Clipboard content (text) received from portal!")
+                logging.info("Clipboard content (text) received from portal!")
                 pyperclip.copy(decrypted_clipboard_content.decode())
             self.last_content_hash = clipboard_hash
 
@@ -88,15 +98,14 @@ class MQTTClientWithClipboard:
 def process_clipboard_content(cipher):
     # Get clipboard contents
     clipboard_content = pyperclip.paste()
-    im = ImageGrab.grabclipboard()
+    image_content = ImageGrab.grabclipboard()
     content_type = 'text'
-    if im is not None:
-        if isinstance(im, PngImageFile):
-            with io.BytesIO() as output:
-                im.save(output, format="PNG")
-                image_data = output.getvalue()
-                clipboard_content = base64.b64encode(image_data).decode()
-            content_type = 'image'
+    if image_content and isinstance(image_content, PngImageFile):
+        with io.BytesIO() as output:
+            image_content.save(output, format="PNG")
+            image_data = output.getvalue()
+            clipboard_content = base64.b64encode(image_data).decode()
+        content_type = 'image'
     
     # Skip if clipboard_content is blank
     if content_type == 'text' and not clipboard_content.strip():
@@ -105,27 +114,23 @@ def process_clipboard_content(cipher):
     clipboard_data = clipboard_content.encode()
     clipboard_hash = hashlib.sha256(clipboard_data).hexdigest()
     encrypted_content_str = cipher.encrypt(clipboard_data).decode()
-    clipboard_payload = ClipboardPayload(clipboard_hash, content_type, encrypted_content_str)
-
-    return clipboard_payload
+    return ClipboardPayload(clipboard_hash, content_type, encrypted_content_str)
 
 
 class ClipboardMonitor(threading.Thread):
-    def __init__(self, mqttc_wrapper, topic, cipher):
+    def __init__(self, mqttc_wrapper):
         super().__init__()
         self.mqttc_wrapper = mqttc_wrapper
-        self.topic = topic
-        self.cipher = cipher
         self.interval = 2
         self.running = True
 
     def run(self):
         while self.running:
-            clipboard_payload = process_clipboard_content(self.cipher)
+            clipboard_payload = process_clipboard_content(self.mqttc_wrapper.cipher)
             if clipboard_payload and clipboard_payload.hash != self.mqttc_wrapper.last_content_hash:
                 # Publish clipboard contents to MQTT broker
-                self.mqttc_wrapper.client.publish(self.topic, clipboard_payload.to_json())
-                print(f"Clipboard content ({clipboard_payload.type}) sent to portal!")
+                self.mqttc_wrapper.client.publish(self.mqttc_wrapper.topic, clipboard_payload.to_json())
+                logging.info(f"Clipboard content ({clipboard_payload.type}) sent to portal!")
                 self.mqttc_wrapper.last_content_hash = clipboard_payload.hash
 
             time.sleep(self.interval)  # Local clipboard monitoring interval
@@ -135,10 +140,18 @@ class ClipboardMonitor(threading.Thread):
         self.running = False
 
 
+def setup_mqtt_connection(mqttc_wrapper, broker_address, broker_port, no_recv=False):
+    mqttc_wrapper.no_recv = no_recv
+    mqttc_wrapper.client.connect(broker_address, broker_port, 60)
+    mqttc_wrapper.client.loop_start()
+    time.sleep(2)  # Wait for a short while to receive messages
+    return mqttc_wrapper
+
+
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument('mode', choices=['send', 'receive', 'sync'], help='Mode of operation')
     parser.add_argument('--version', action='version', version='%(prog)s ' + __version__)
-    parser.add_argument('--mode', choices=['send', 'receive', 'sync'], help='Mode of operation', default='sync')
 
     args = parser.parse_args()
 
@@ -151,7 +164,6 @@ def main():
 
     # Prompt the user for a password
     password = getpass("Enter a password to encrypt & decrypt the clipboard content: ")
-
     # Create a cipher object
     cipher = Fernet(create_key_from_password(password, topic))
 
@@ -160,27 +172,18 @@ def main():
     mqttc_wrapper = MQTTClientWithClipboard(mqttc, topic, cipher)
 
     if args.mode == 'send':
+        mqtt_connection = setup_mqtt_connection(mqttc_wrapper, broker_address, broker_port, True)
         clipboard_payload = process_clipboard_content(cipher)
-        mqttc.connect(broker_address, broker_port, 60)
-        mqttc.publish(topic, clipboard_payload.to_json(), retain=True)
-        mqttc.disconnect()
+        mqtt_connection.client.publish(topic, clipboard_payload.to_json(), retain=True)
+        logging.info(f"Clipboard content ({clipboard_payload.type}) sent to portal!")
     elif args.mode == 'receive':
-        mqttc.on_message = mqttc_wrapper.on_message
-        mqttc.connect(broker_address, broker_port, 60)
-        mqttc.subscribe(topic)
-        mqttc.loop_start()
-        time.sleep(2)  # wait for a short while to receive messages
-        mqttc.loop_stop()
-        mqttc.disconnect()
+        mqtt_connection = setup_mqtt_connection(mqttc_wrapper, broker_address, broker_port)
     else:  # args.mode == 'sync'
         try:
-            mqttc_wrapper.client.connect(broker_address, broker_port, 60)
-
-            # Start the MQTT loop
-            mqttc_wrapper.client.loop_start()
+            mqtt_connection = setup_mqtt_connection(mqttc_wrapper, broker_address, broker_port)
 
             # Start the clipboard monitor
-            clipboard_monitor = ClipboardMonitor(mqttc_wrapper, topic, cipher)
+            clipboard_monitor = ClipboardMonitor(mqtt_connection)
             clipboard_monitor.start()
 
             while True:
@@ -192,9 +195,9 @@ def main():
             clipboard_monitor.stop()
             clipboard_monitor.join()
 
-            # Stop the MQTT loop and disconnect from the broker
-            mqttc.loop_stop()
-            mqttc.disconnect()
+    # Stop the MQTT loop and disconnect from the broker
+    mqttc.loop_stop()
+    mqttc.disconnect()
 
 if __name__ == "__main__":
     main()
